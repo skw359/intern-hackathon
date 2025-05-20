e09c62141ea9462b4553af3c3138bd1e7de7eb6d
KtokerAV <kevin.toker@anavationllc.com>
2025-05-20 09:05:25 -0400

    Add header with YouWork logo

--- frontend/src/Home/home.js
+++ frontend/src/Home/home.js
@@ -5,15 +5,20 @@ import "./home.css"
 
 export default function Home() {
   return (
-    <div className="calendar-container">
-      <FullCalendar
-        plugins={[dayGridPlugin]}
-        initialView="dayGridMonth"
-        events={[
-          { title: 'Meeting', date: '2025-05-21' },
-          { title: 'Launch', date: '2025-05-25' },
-        ]}
-      />
-    </div>
+    <>
+      <header className="header">
+        <h1 className="header-logo">YouWork</h1>
+      </header>
+      <div className="calendar-container">
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          events={[
+            { title: 'Meeting', date: '2025-05-21' },
+            { title: 'Launch', date: '2025-05-25' },
+          ]}
+        />
+      </div>
+    </>
   )
 }
