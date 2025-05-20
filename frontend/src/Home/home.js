192d36bba6bfc3c29695f45c66424f234c8c6256
KtokerAV <kevin.toker@anavationllc.com>
2025-05-20 09:07:41 -0400

    Add personalized greeting above calendar

--- frontend/src/Home/home.js
+++ frontend/src/Home/home.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import "./home.css"
 
 export default function Home() {
+  const [name] = useState('John') // This would typically come from your authentication state
+
   return (
     <>
       <header className="header">
         <h1 className="header-logo">YouWork</h1>
       </header>
       <div className="calendar-container">
+        <h2 className="greeting">Hello {name}, this is your workout schedule</h2>
         <FullCalendar
           plugins={[dayGridPlugin]}
           initialView="dayGridMonth"
