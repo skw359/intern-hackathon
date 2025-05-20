10fcf52f192e52e772e58d31d660d1bf54cb3dd8
KtokerAV <kevin.toker@anavationllc.com>
2025-05-20 15:07:37 -0400

    Updated api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -12,7 +12,7 @@ api.interceptors.request.use(
   config => {
     const token = localStorage.getItem('token');
     if (token) {
-      config.headers.authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
