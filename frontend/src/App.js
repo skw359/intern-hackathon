8d7138ee31d014ea1561aa30ec1549cc9d04947d
KtokerAV <kevin.toker@anavationllc.com>
2025-05-20 15:57:51 -0400

    Add Protected Route Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './Login/login'
 import Register from './Register/register'
 import Home from './Home/home';
+import ProtectedRoute from './components/ProtectedRoute';
 
 import './App.css';
 
@@ -11,7 +12,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
+        <Route 
+          path="/home" 
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          } 
+        />
       </Routes>
     </Router>
   );
