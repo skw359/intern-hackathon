663163789f86fcceef3bdc52a5482588bbda1aa2
KtokerAV <kevin.toker@anavationllc.com>
2025-05-20 12:43:44 -0400

    Update API service to include token and fix workout loading

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,6 +8,20 @@ const api = axios.create({
   }
 });
 
+// Add request interceptor to include token
+api.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
 api.interceptors.response.use(
   response => response,
   error => {
