19db35b2c6482a71e3f4f44d0ec3c407c4391a4c
KtokerAV <kevin.toker@anavationllc.com>
2025-05-20 13:50:32 -0400

    Parse calendar dates

--- frontend/src/Home/home.js
+++ frontend/src/Home/home.js
@@ -106,7 +106,7 @@ export default function Home() {
     { title: 'Launch', date: '2025-05-25' },
     ...workouts.map(workout => ({
       title: workout.description || workout.title,
-      date: workout.date
+      date: workout.date.split('T')[0]
     }))
   ]
 
